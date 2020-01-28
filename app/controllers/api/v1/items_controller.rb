56753e7ae32e1722a0d9eb06e19f30f2a366befb
github username <github email address>
2020-01-28 13:13:07 -0800

    fix syntax

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ItemsController < ApplicationController
-    skip_before_action :authorized, only [:index, :show]
+    skip_before_action :authorized, only: [:index, :show]
 
     def index
         items = Item.all 
